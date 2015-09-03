8dbc220414efd1b4bed819ab27ededaee6bf3ff1
Matthew <matthew@laptop.matthewtwo.com>
2015-09-03 03:27:36 -0500

    Working code

--- Robot_test.ino
+++ Robot_test.ino
@@ -12,6 +12,8 @@
 #define motor2_pin1 10
 #define motor2_pin2 11
 
+bool debug = false;
+
 unsigned long steerRaw;
 unsigned long throttleRaw;
 
@@ -42,7 +44,9 @@ void setup()
   pinMode(motor2_enable, OUTPUT);
 
 
-  Serial.begin(9600);
+  if(debug) {
+    Serial.begin(9600);
+  }
 }
 void getInputs() {
   steerRaw = pulseIn(steeringInput, HIGH);
@@ -74,52 +78,91 @@ void setDirection(int motor1_direction, int motor2_direction) {
 
 void loop() {
   getInputs();
- // Serial.print("steerIn: ");
- // Serial.println(steerIn);
-  // Serial.print("throttleIn: ");
-  // Serial.println(throttleIn); 
-
-
-
 
   if(throttleIn>=input_buffer) {
-    Serial.println("Forwards!");
     setDirection(FORWARD, FORWARD);
     motor1_output = throttleIn;
     motor2_output = throttleIn;
 
     if(steerIn >= input_buffer) {
-      motor1_output = motor1_output - steerIn;
+      if(debug) {
+        Serial.println("Fwd + Right");
+      }
+      motor1_output -= steerIn;
     } else if(steerIn <= -input_buffer) {
-      motor2_output = motor2_output + steerIn;
+      if(debug) {
+        Serial.println("Fwd + Left");
+      }
+      motor2_output += steerIn;
+    } else {
+      if(debug) {
+        Serial.println("Forwards!");
+      }
     }
-        
 
-  } else if(throttleIn<= -input_buffer) {
-    Serial.println("Backwards!");
+  } else if(throttleIn <= -input_buffer) {
     setDirection(BACKWARD, BACKWARD);
     motor1_output = abs(throttleIn);
     motor2_output = abs(throttleIn);
+
+    if(steerIn >= input_buffer) {
+      if(debug) {
+        Serial.println("Bk + Right");
+      }
+      motor2_output -= steerIn;
+
+    } else if(steerIn <= -input_buffer) {
+      if(debug) {
+        Serial.println("Bk + Left");
+      }
+      motor1_output += steerIn;
+
+    } else {
+      if(debug) {
+      Serial.println("Backwards!");
+      }
+    }
+    if(debug) {
+      Serial.print("motor1_output: ");
+      Serial.println(motor1_output);
+    }
   } else {
     // Serial.println("Stationary!");
     motor1_output = motor2_output = 0;
 
     if(steerIn >= input_buffer) {
       // Turn right
-      Serial.println("Turn right!!");
+      if(debug) {
+        Serial.println("Turn right!!");
+      }
+      setDirection(BACKWARD, FORWARD);
+      motor1_output = motor2_output = steerIn;
+
     } else if(steerIn <= -input_buffer) {
-      Serial.println("Turn left!");
+      if(debug) {
+        Serial.println("Turn left!");
+      }
+      setDirection(FORWARD, BACKWARD);
+      motor1_output = motor2_output = abs(steerIn);
+
     }
 
   }
 
     
  
-
+  // Don't try outputing negative pwm values.
+  if(motor1_output < 0) {
+    motor1_output = 0;
+  }
+  if(motor2_output < 0) {
+    motor2_output = 0;
+  }
   analogWrite(motor1_enable, motor1_output);
   analogWrite(motor2_enable, motor2_output);
   
   // motor1_output = motor2_output = 0;
-
-//  delay(500);
+  if(debug) {
+    delay(500);
+  }
 }
